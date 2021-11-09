b273f3a41e681e41e108e8765467f98b820fb385
김범석 <kqjatjr@gmail.com>
2021-11-10 00:30:47 +0900

    리액트todoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoTemplate from "./components/TodoTemplate";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <TodoTemplate>Todo APP을 만들어보자!</TodoTemplate>;
 }
 
 export default App;
