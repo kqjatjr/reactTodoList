9d18597fe4865e250106fe35906fd24a6ed77c51
김범석 <kqjatjr@gmail.com>
2021-11-10 02:24:36 +0900

    todoList Template

--- src/components/TodoTemplate.tsx
+++ src/components/TodoTemplate.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import "../CSS/TodoTemplate.scss";
 
 type props = {
-  children: JSX.Element;
+  children: JSX.Element[];
 };
 
-const TodoTemplate = (props: props) => {
+const TodoTemplate = ({ children }: props) => {
   return (
     <div className="TodoTemplate">
       <div className="app-title">일정 관리</div>
-      <div className="content">{props.children}</div>
+      <div className="content">{children}</div>
     </div>
   );
 };
